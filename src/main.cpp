2f1ebf3d3f920f706e9ec5199f47a69e2f4a7bd5
Dhanoop Karunakaran <dhanoop.karunakaran@gmail.com>
2018-05-29 15:18:30 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -281,6 +281,11 @@ int main() {
 								
 								//Both apparach has pros and cons. The combination of these two can have powerful features. In hybrid approach, multi model alogorithm 
 								//gets replace with a machine learning algorithm. In the lecture, we learned about naive bayes classifier.
+								
+								//In the practice section of the classifier, we learned about few things:
+								//1) We have trained datset where X represet the coordinates(s,d s_dot, d_dot) and Y represent the corresponding labels(left, right, straight).
+								//2) train the model using the equation of the GNB
+								//3) Predict the new label(Y) when new features(X) sre provoded
 								check_car_s += ((double)prev_size*0.02*check_speed);
 								if(check_car_s > car_s && check_car_s - car_s < 30) {
 									car_ahead = true;										
